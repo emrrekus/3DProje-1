5132dc07c327cac3a46cd8016d976b446412cef6
Emre <112270755+emrrekus@users.noreply.github.com>
2022-12-23 21:16:36 +0300

    level eklendi

--- 3DProje 1/Assets/GameFolders/Scripts/Concretes/Controllers/MoverWall.cs
+++ 3DProje 1/Assets/GameFolders/Scripts/Concretes/Controllers/MoverWall.cs
@@ -8,14 +8,13 @@ namespace Proje1.Controllers
     public class MoverWall : WallController
     {
         [SerializeField] Vector3 _direction;
-     
-        [SerializeField] float _factor;
-        [SerializeField] float _speed=1f;  
+        [SerializeField] float _speed = 1f;
         
+
 
         Vector3 _startPosition;
         private const float FULL_CIRCLE = Mathf.PI * 2f;
-       
+        float _factor;
 
         private void Awake()
         {
@@ -25,13 +24,14 @@ namespace Proje1.Controllers
         {
 
             float cycle = Time.time / _speed;
-            float sinWave = Mathf.Sin(cycle * FULL_CIRCLE) ;
+            float sinWave = Mathf.Sin(cycle * FULL_CIRCLE);
 
             _factor = Mathf.Abs(sinWave);
+            _factor= sinWave /2f+0.5f;
             Vector3 offset = _direction * _factor;
             transform.position = offset + _startPosition;
 
-            
+
         }
     }
 
